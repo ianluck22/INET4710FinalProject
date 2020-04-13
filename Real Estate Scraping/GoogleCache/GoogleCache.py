c7c318b24ca829c8e3c91886f0804c8a9999d7eb
Prateek Vachher <vachh007@umn.edu>
2020-04-13 18:22:16 -0500

    Added DataFiles for processing

--- Real Estate Scraping/GoogleCache/GoogleCache.py
+++ Real Estate Scraping/GoogleCache/GoogleCache.py
@@ -1,53 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import os, time, csv
+import pandas as pd
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
-def read_csv():
-    list1 = []
-    file = '../../propertyTrans.csv'
-    with open(file) as fh:
-        rd = csv.DictReader(fh, delimiter=',')
-        for row in rd:
-            list1.append(row)
-    return list1
+FILE_NUMBER = 0
+
+df = pd.read_feather(f"../../datafiles/metroAreaChunks/metroArea{FILE_NUMBER}.ft")
+list_of_addresses = df[df["GoogleCacheLink"] == ""]["property"].to_list()
 
 driver = webdriver.Chrome()
 
-with open('data.csv', 'w', newline='') as csvfile:
-    fieldnames = open("../../propertyTrans.csv", 'r').readline()[:-1].split(',')
-    fieldnames.append("cacheLink")
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+for address in tqdm(list_of_addresses):
+    count_retry = 0
+    while True:
+        try:
+            count_retry += 1
+            search_term = f"{address}"
+            driver.get("http://www.google.com/search?q="+search_term.replace(' ', '%20'))
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            cacheLink = ""
+            for link in soup.find_all('li'):
+                for a in link.find_all('a'):
+                    if 'webcache' in a.get('href') and 'zillow' in a.get('href'):
+                        driver.get(a.get('href'))
+                        cacheLink = a.get("href")
 
-    for row in tqdm(read_csv()):
-        count_retry = 0
-        while True:
-            try:
-                count_retry += 1
-                search_term = f"{row['addressLine1']} {row['stateOrProvince']} {row['zip']}"
-                driver.get("http://www.google.com/search?q="+search_term.replace(' ', '%20'))
-                soup = BeautifulSoup(driver.page_source, 'html.parser')
+            df.loc[df['property'] == address, ['GoogleCacheLink']] = cacheLink
+            break
 
-                for link in soup.find_all('li'):
-                    for a in link.find_all('a'):
-                        if 'webcache' in a.get('href') and 'zillow' in a.get('href'):
-                            driver.get(a.get('href'))
-                            row['cacheLink'] = a.get("href")
-
-                if 'cacheLink' not in row:
-                    row['cacheLink'] = ""
-                
-                writer.writerow(row) 
+        except Exception as e:
+            print ("Retrying because of", str(e))
+            if count_retry < 3:
+                time.sleep(5)
+                continue
+            else:
+                cacheLink
                 break
-            except Exception as e:
-                print ("Retrying because of", str(e))
-                if count_retry < 3:
-                    time.sleep(5)
-                    continue
-                else:
-                    row['cacheLink'] = ""
-                    writer.writerow(row) 
-                    break
+
+    df.to_feather(f"../../datafiles/metroAreaChunks/metroArea{FILE_NUMBER}.ft")
+
 driver.close()
