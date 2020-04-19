ed43b3e02ba42db23baddeaa9f87a546d6cb4ab5
Prateek Vachher <vachh007@umn.edu>
2020-04-19 01:36:53 -0500

    Processed Bing Scraped Files 0, 1, 2

--- Real Estate Scraping/duckduckgo.py
+++
@@ -1,6 +0,0 @@
-import rubberduck
-
-def getCacheResponse(url):
-    response = rubberduck.search(url, bang='cache', no_redirect=0, skip_disambig=0)
-    result = response.content
-    return result
--- Real Estate Scraping/processCache.py
+++ Real Estate Scraping/processCache.py
@@ -1,6 +1,13 @@
-import os, time, csv, json
+import os, time, csv, json, rubberduck, sys, pickle, random
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+
+
+def getCacheResponse(url):
+    response = rubberduck.search(url, bang='cache', no_redirect=0, skip_disambig=0)
+    result = response.content
+    return result
+
 
 def getInformation(data):
     soup = BeautifulSoup(data, 'html.parser')
@@ -38,3 +45,24 @@ def getInformation(data):
 
     return payload
 
+FILE_NUMBER = sys.argv[1]
+df = pickle.load(open(f"../datafiles/BingScraped/metroArea{FILE_NUMBER}.ft", "rb"))
+list_of_urls = df[df["status"] == "NEW"]["ZillowURL"].to_list()
+
+for url in tqdm(list_of_urls):
+    content = getCacheResponse(url)
+    try:
+        payload = getInformation(content)
+        del payload['listing_sub_type']
+        for x in payload:
+            try:
+                df.loc[df['ZillowURL'] == url, [x]] = payload[x]
+            except:
+                pass
+        df.loc[df['ZillowURL'] == url, ['status']] = "Completed"
+
+    except Exception as e:
+        df.loc[df['property'] == url, ['status']] = "NEW"
+        print (str(e), content[:50])
+    
+    time.sleep(random.randint(5, 10))
